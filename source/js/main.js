8d47f1247af0aa607c6a62556fdb0bb6861ab9c1
Morfey216 <Morfey4web@gmail.com>
2021-01-06 02:03:53 +0300

    project setup

---
+++ source/js/main.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import './general/polyfills';
+import './general/intersection-observer';
+// import utils from './general/utils';
+/* eslint-enable */
+
+// region Blocks
+
+// import formInput from '../blocks/form-input/form-input';
+// import formTextarea from '../blocks/form-textarea/form-textarea';
+// import header from '../blocks/header/header';
+// import advantages from '../blocks/advantages/advantages';
+// import calculation from '../blocks/calculation/calculation';
+
+// endregion
+
+// formInput();
+// formTextarea();
+// header();
+// advantages();
+// calculation();
